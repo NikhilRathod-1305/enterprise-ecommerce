955d6f8de9775fe95b70dfcab83a6d58fac2aa0b
Nikhil Rathod <nikhilrathod1305@gmail.com>
2026-01-16 09:36:17 +0530

    feat: implement MongoDB connection and user model, update user creation logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,20 +1,14 @@
 import { createUserSchema } from "../validations/user.validation.js"
+import User from '../models/User.js'
 
 export const createUser = async (req , res , next) => {
     try{
-        const {error , value} = createUserSchema.validate(req.body);
-
-        if(error){
-            return res.status(400).json({
-                success: false,
-                message: error.details[0].message
-            });
-        }
-
+        const user = await User.create(req.body);
         res.status(201).json({
-            success:true,
-            message: value
-        });
+            success: true,
+            data: user
+        })
+        
     }
     catch(err){
         next(err);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import app from './app.js';
 import { env } from './config/env.js';
+import connectDB from './config/db.js';
 
 app.listen(env.PORT, () =>{
     console.log(`Server running on port ${env.PORT}`)
 })
+
+connectDB();
