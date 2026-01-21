16385ed6b251803f4abd32e370b860537770e0c3
NikhilRathod-1305 <nikhilrathod1305@gmail.com>
2026-01-21 08:51:37 +0530

    feat: enhance login functionality with refresh token and role-based access control

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -4,35 +4,44 @@ import { env } from '../config/env.js';
 import jwt from 'jsonwebtoken';
 
 export const login = async (req, res, next) => {
-    try{
-        const {email , password} = req.body;
-        const user = await User.findOne({email});
-        if(!user){
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
             return res.status(401).json({
-                success:false,
-                message:"Invalid creds"
+                success: false,
+                message: "Invalid creds"
             });
         }
-        const isMatches = await bcrypt.compare(password,user.password);
-        if(!isMatches){
-             return res.status(401).json({
-                success:false,
-                message:"Invalid creds"
+        const isMatches = await bcrypt.compare(password, user.password);
+        if (!isMatches) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid creds"
             });
         }
 
         const token = jwt.sign(
-            {id:user._id,role:user.role},
+            { id: user._id, role: user.role },
             process.env.JWT_SECRET,
-            {expiresIn:process.env.JWT_EXPIRES_IN}
+            { expiresIn: process.env.JWT_EXPIRES_IN }
         );
-
+        const refresh = jwt.sign(
+            { id: user._id },
+            process.env.JWT_REFRESH_SECRET,
+            { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN }
+        );
+        res.cookie('refreshToken', refresh, {
+            httpOnly: true,
+            secure: false,
+            SameSite: 'strict'
+        })
         res.json({
-            sucess:true,
+            sucess: true,
             token
         })
     }
-    catch(err){
+    catch (err) {
         next(err);
     }
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/user.controller.js';
 import { login } from '../controllers/login.controller.js';
 import protect from '../middlewares/auth.middleware.js';
+import authorize from '../middlewares/role.middleware.js';
 
 const router = express.Router();
 
@@ -9,7 +10,7 @@ router.post('/users', createUser);
 router.get('/users', protect, getUsers);
 router.get('/users/:id', getUserById);
 router.put('/users/:id', updateUser);
-router.delete('/users/:id', deleteUser);
+router.delete('/users/:id',protect,authorize('admin'), deleteUser);
 router.post('/login',login)
 
 export default router;
